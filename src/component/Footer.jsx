dee23a95a576760ce00322302a3240d44b462423
Jamil115 <jamil71001@gmail.com>
2025-02-08 00:47:55 +0600

    copyright added in footer section

---
+++ src/component/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { PiCopyright } from 'react-icons/pi'
+
+export default function Footer() {
+    return (
+        <div className="flex justify-center py-4">
+                <p className="flex text-xl text-gray-400">TM <span className="pt-1 px-1"><PiCopyright /></span> 2024 - Jamil Khan Riyed - All Rights Reserved.</p>
+        </div>
+    )
+}
