483e887caa3e5bc16daac222e8bcada36a71df79
Jamil115 <jamil71001@gmail.com>
2025-02-08 00:54:34 +0600

    homepage created successfully

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../component/Navbar'
+import Hero from '../component/Hero'
+import Footer from '../component/Footer'
+
+export default function Home() {
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+            <Footer />
+        </div>
+    )
+}
