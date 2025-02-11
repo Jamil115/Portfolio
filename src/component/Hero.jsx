b409852c2ad7fda3c9021a473d063f27efdb9e11
Jamil115 <jamil71001@gmail.com>
2025-02-11 15:04:48 +0600

    add a border and modify the code

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -5,17 +5,17 @@ import { VscGithub } from "react-icons/vsc";
 
 export default function Hero() {
   return (
-    <div className="flex flex-col justify-center items-center h-[800px]">
+    <div className="flex flex-col justify-center items-center h-[730px] border-2 rounded-4xl mx-64 my-8">
         <div className="avatar mb-8">
             <div className="ring-primary ring-offset-base-100 w-52 rounded-full ring ring-offset-2">
-                <img src="../../public/jk.png"/>
+                <img src="../../public/jamil.png"/>
             </div>
         </div>
         <div className="text-center">
             <p className="text-2xl mb-4 font-semibold">Hello! I'm </p>
-            <h1 className="font-bold text-5xl mb-4 text-yellow-300">Jamil Khan Riyed</h1>
+            <h1 className="font-bold text-5xl mb-4 text-yellow-300 font-serif">Jamil Khan Riyed</h1>
             <p className="text-xl font-semibold mb-7">I'm a <span className="bg-[#2c7852] text-white p-1">MERN Stack</span> Developer</p>
-            <p className="text-gray-300 text-xl mx-52 px-96 h-20">I am a passionate fresher in front-end web development, skilled in HTML, CSS, and JavaScript. I enjoy creating responsive, visually appealing, and user-friendly websites</p>
+            <p className="text-gray-300 text-xl mx-40 px-36 h-20">I am a passionate fresher in front-end web development, skilled in HTML, CSS, and JavaScript. I enjoy creating responsive, visually appealing, and user-friendly websites</p>
         </div>
         <div className="flex gap-10 text-3xl mt-2">
             <a href="https://www.facebook.com/Jamilkhan.jk7/"><FaFacebook /></a>
