4961a649157942d8cc62d4d100e674f6614a731a
Jamil115 <jamil71001@gmail.com>
2025-02-11 14:59:12 +0600

    add all pages path

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Skills from "./pages/Skills";
+import Education from "./pages/Education";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+
+function App() {
+  let allRoute = createBrowserRouter(
+    [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/skills',
+        element: <Skills />
+      },
+      {
+        path: '/education',
+        element: <Education />
+      },
+      {
+        path: '/projects',
+        element: <Projects />
+      },
+      {
+        path: '/cantact',
+        element: <Contact />
+      }
+    ]
+  )
+  return (
+    <>
+      <RouterProvider router={allRoute} />
+    </>
+  );
+}
+
+export default App;
