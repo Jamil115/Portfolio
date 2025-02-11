96bddc03716b74f6e077091d11316ba17a5bfaa6
Jamil115 <jamil71001@gmail.com>
2025-02-11 15:19:18 +0600

    added project page

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import Navbar from '../component/Navbar'
+import ProjectCard from '../component/ProjectCard'
+import Footer from '../component/Footer'
+
+export default function Projects() {
+    const info = [
+        {
+            title: "Shoppy - An E-commerce Website",
+            description: "The website has a good looking user interface. Customers of this website can get the latest fashion newsletters, blogs. Anyone can buy their necessary things from this website.",
+            button: [
+                {
+                    type: "Live",
+                    link:"https://shoppy-bice-six.vercel.app/"
+                }, 
+                {
+                    type:"Github", 
+                    link:"https://github.com/Jamil115/Shoppy"}
+                ],
+            image: "../../public/shoppy.png"
+        },
+        {
+            title: "BookShop",
+            description: "This is a backend project with a simple frontend. JWT verification has been done for both server side and client side. Fetching books information from database. The password will be securely stored as a hashed value using the bcrypt function.",
+            button: [
+                {
+                    type:"Github", 
+                    link:"https://github.com/Jamil115/BookStore"
+                }
+            ],
+            image: "../../public/bookshop.png"
+        },
+        {
+            title: "MovieFlix - Movie Search Website ",
+            description: "TIntegrated the OMDB API to fetch and display real-time movie data based on user input. Handled API requests efficiently with JavaScript, managing data fetching and display dynamically without page reloads.",
+            button: [
+                {
+                    type:"Github", 
+                    link:"https://github.com/Jamil115/Movie-Search"
+                }
+            ],
+            image: "../../public/moveflix.png"
+        }
+    ]
+
+    console.log(info[0])
+    return (
+        <div>
+            <Navbar />
+            <p className="text-center mt-16 text-3xl font-semibold font-serif border-2 border-slate-400 rounded mx-[600px] mb-20">My Projects</p>
+            <ProjectCard info = {info}/>
+        </div>
+    )
+}
