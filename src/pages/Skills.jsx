4dc6577bb5f6a347dcee83f10c7d60469f889201
Jamil115 <jamil71001@gmail.com>
2025-02-12 00:56:49 +0600

    added gap between cards

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -40,7 +40,7 @@ export default function Skills() {
                 <div>
                     <p className="text-center mt-12 text-xl font-semibold font-serif border-2 border-slate-400 rounded mx-[350px] ">Tools</p>
                     <p className="text-center mt-7 font-light font-mono text-neutral-300">Necessary Tools That I Am Familiar With</p>
-                    <div className="grid grid-cols-4 justify-items-center mx-64 my-20">
+                    <div className="grid grid-cols-4 gap-10 justify-items-center mx-64 my-20">
                         <Card image={canva} name={"Canva"} />
                         <Card image={vscode} name={"VS Code"} />
                         <Card image={photoshop} name={"Photoshop"} />
